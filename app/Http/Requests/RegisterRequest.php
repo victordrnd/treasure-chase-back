18529811302c17215054721791c8e66fba1073da
Victor <vic20016@gmail.com>
2021-02-03 14:54:57 +0100

    fix: register request

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -26,7 +26,7 @@ class RegisterRequest extends FormRequest
         return [
             'firstname' => 'required|string',
             'lastname' => 'required|string',
-            'email' => 'required|email',
+            'email' => 'required|email|unique:users,email',
             'password' => 'string|required|min:5'
         ];
     }
