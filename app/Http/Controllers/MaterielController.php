44d18a9a59b69867827745db641e76d04e215965
Victor <vic20016@gmail.com>
2021-10-10 18:23:43 +0200

    feat: cache

--- app/Http/Controllers/MaterielController.php
+++ app/Http/Controllers/MaterielController.php
@@ -10,22 +10,25 @@ use App\Models\Materiel;
 use App\Models\MaterielCategory;
 use App\Models\Pull;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class MaterielController extends Controller {
     public function list() {
-        $forfaits = Forfait::all();
-        $pulls = Pull::all();
-        $foodpack = FoodPack::all();
-        $materiel_categories = MaterielCategory::with('materiels')->get();
-        $materiels = Materiel::whereNull('materiel_category_id')->get();
-        $assurances = Assurance::all();
-        return new MaterielsResource(array(
-            'forfaits' => $forfaits,
-            'pulls' => $pulls,
-            'foodpacks' => $foodpack,
-            'materiel_categories' => $materiel_categories,
-            'assurances' => $assurances,
-            'materiels' => $materiels
-        ));
+        return Cache::rememberForever('materiels', function () {
+            $forfaits = Forfait::all();
+            $pulls = Pull::all();
+            $foodpack = FoodPack::all();
+            $materiel_categories = MaterielCategory::with('materiels')->get();
+            $materiels = Materiel::whereNull('materiel_category_id')->get();
+            $assurances = Assurance::all();
+            return new MaterielsResource(array(
+                'forfaits' => $forfaits,
+                'pulls' => $pulls,
+                'foodpacks' => $foodpack,
+                'materiel_categories' => $materiel_categories,
+                'assurances' => $assurances,
+                'materiels' => $materiels
+            ));
+        });
     }
 }
