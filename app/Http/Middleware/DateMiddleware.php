8ea776e9971dcc3a0589eaf3b2193cd2a0be8e5f
Victor <vic20016@gmail.com>
2021-10-25 20:16:27 +0200

    fix: admin access

--- app/Http/Middleware/DateMiddleware.php
+++ app/Http/Middleware/DateMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Carbon\Carbon;
 use Closure;
 use Illuminate\Http\Request;
@@ -17,6 +18,12 @@ class DateMiddleware
      */
     public function handle(Request $request, Closure $next, $date)
     {
+        if($request->input("email", false)){
+            $user = User::where("email", $request->email)->first();
+            if($user->is_admin){
+                return $next($request);
+            }
+        }
         $date = Carbon::parse($date);
         if($date > Carbon::now()){
             return response()->json(['errors' => ["Le service sera accessible ".$date->diffForHumans()]], 401);
